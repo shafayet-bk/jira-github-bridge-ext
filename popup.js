1445dfcd130cea7ff2e1812f70c5bc2dfac56c7c
shafayet-bk <sayem.shafayet@bkash.com>
2021-06-16 17:00:50 +0600

    Add working code

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-// Initialize button with user's preferred color
+// Initialize butotn with users's prefered color
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
@@ -15,7 +15,7 @@ changeColor.addEventListener("click", async () => {
   });
 });
 
-// The body of this function will be executed as a content script inside the
+// The body of this function will be execuetd as a content script inside the
 // current page
 function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
