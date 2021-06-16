23803108e42afcf93c272901d11e4aadaacb3584
shafayet-bk <sayem.shafayet@bkash.com>
2021-06-16 18:31:24 +0600

    Detect on more places

--- process.js
+++ process.js
@@ -1,10 +1,13 @@
 console.log("Processor script loaded.");
 
-const INITIAL_DELAY = 1000;
+const INITIAL_DELAY = 500;
 const SECONDARY_DELAY = 2000;
 const MAX_ATTEMPTS = 3;
-const QUERY_STRING =
-  "a.Link--primary.text-bold.js-navigation-open.markdown-title";
+
+const QUERY_STRING = "a.Link--primary";
+const QUERY_STRING_2 = "a.Link--secondary";
+
+// const QUERY_STRING_3 = "a.Link--primary";
 
 const extractStoryId = (el) => {
   let text = el.innerHTML;
@@ -23,7 +26,7 @@ const makeAnchor = (storyId) => {
   let href = makeJiraLinkUrl(storyId);
 
   let newEl = document.createElement("a");
-  newEl.innerHTML = "Open Jira Story";
+  newEl.innerHTML = "[Jira]";
   newEl.href = href;
   newEl.target = "_blank";
 
@@ -34,7 +37,13 @@ let attemptCount = 0;
 const createJiraLinks = () => {
   attemptCount += 1;
 
-  let elList = document.querySelectorAll(QUERY_STRING);
+  let elList = [].concat(
+    Array.from(document.querySelectorAll(QUERY_STRING)),
+    Array.from(document.querySelectorAll(QUERY_STRING_2))
+    // Array.from(document.querySelectorAll(QUERY_STRING_3))
+  );
+  elList = [...new Set(elList)];
+  console.log({ elList });
 
   // retry if initial attempt failed.
   if (elList.length == 0) {
